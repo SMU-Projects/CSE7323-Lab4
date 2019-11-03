76687bf86945bf8f2b31efdf5ff2247c41b466f9
Will Lacey <WillLacey@willlaceysmbp4.home>
2019-11-03 11:34:10 -0600

    Initial Commit of Project; Realllllllly starting late here

---
+++ Lab4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab4
+//
+//  Created by Will Lacey on 11/3/19.
+//  Copyright © 2019 Will Lacey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
